59087f393e94c9976ff332b8183788a37e2c05d1
sarbajit <sarbajitchatterjee09@gmail.com>
2025-12-02 11:35:00 +0100

    modifiied overview | min 24

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -12,6 +12,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+  # ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  # instance_type = "t2.micro"
+
+  # new based on Ubuntu 24.04 in Nov 2025.
+  ami           = "ami-0fa3fe0fa7920f68e"
+  instance_type ="t3.micro"
 }

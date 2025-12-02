f8ff90278be088b00df65287152a2d0a0d7662ce
sarbajit <sarbajitchatterjee09@gmail.com>
2025-12-02 13:06:28 +0100

    creating state file on s3 | min 44

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -11,11 +11,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  # ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  # instance_type = "t2.micro"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "devops-directive-tf-state"
+  force_destroy = true
+  versioning {
+    enabled     = true
+  }
 
-  # new based on Ubuntu 24.04 in Nov 2025.
-  ami           = "ami-0fa3fe0fa7920f68e"
-  instance_type ="t3.micro"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "Lockid"
+
+  attribute {
+    name = "Lockid"
+    type = "S"
+  }
 }

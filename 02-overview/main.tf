aacef27e83f592ee22c785c69cad2c6720f4f75e
sarbajit <sarbajitchatterjee09@gmail.com>
2025-12-02 15:14:04 +0100

    modifying state file on s3 | min 44

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "my-devops-directive-tf-state"
+  bucket        = "my-learning-directive-tf-state"
   force_destroy = true
   versioning {
     enabled     = true

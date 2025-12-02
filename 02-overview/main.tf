501cd03494d21afecf8e6199f069e5c84d754a79
sarbajit <sarbajitchatterjee09@gmail.com>
2025-12-02 13:17:08 +0100

    modifying state file on s3 | min 44

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "devops-directive-tf-state"
+  bucket        = "myDevops-directive-tf-state"
   force_destroy = true
   versioning {
     enabled     = true
@@ -27,6 +27,26 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 
 }
+
+# Work-in-progress below
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "devops-directive-tf-state"
+#   force_destroy = true
+#   versioning {
+#     enabled = true
+#   }
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+#   bucket = aws_s3_bucket.devops-directive-tf-state.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.mykey.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locking"
